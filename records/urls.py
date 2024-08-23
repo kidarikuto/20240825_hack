0db87e24742776c22b493f1b49154e9efff87588
kidarikuto <tan04tan20@gmail.com>
2024-08-23 23:34:50 +0900

    first commit kida

--- records/urls.py
+++ records/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('status/', views.LabStatusView.as_view(), name='lab_status'),
     path('enter/', views.EnterLabView.as_view(), name='enter_lab'),
     path('exit/', views.ExitLabView.as_view(), name='exit_lab'),
+    path('graph/', views.LogGraphView.as_view(), name='log_graph'),
 ]
