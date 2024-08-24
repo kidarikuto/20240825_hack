7fa5dda758be776e2c85f7fea62c79208c296451
harune-pg <h.makino.22.6.533@4nonome.com>
2024-08-24 09:05:05 +0900

    Userモデルに顔写真を追加

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,10 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+def image_path(instance, filename):
+    return f"images/{instance.username}.{filename.split('.')[-1]}"
+
+
 class User(AbstractUser):
     email = models.EmailField(blank=True, null=True)
+    face_image = models.ImageField(upload_to=image_path, blank=True, null=True)
