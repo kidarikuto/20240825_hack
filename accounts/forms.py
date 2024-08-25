900413dbed604795bf9a036fc5781f0ccca94149
harune-pg <h.makino.22.6.533@4nonome.com>
2024-08-25 13:08:41 +0900

    ログイン・ユーザー登録・ナビゲーションバー

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,10 +4,21 @@ from django import forms
 
 User = get_user_model()
 
-
 class SignupForm(UserCreationForm):
-    face_image = forms.ImageField(required=False, help_text='顔写真をアップロードしてください。')
+    face_image = forms.ImageField(
+        required=False, 
+        help_text='顔認証ログイン用の顔写真をアップロードしてください。',
+        widget=forms.ClearableFileInput(attrs={'class': 'form-control', 'accept': 'image/*'})
+    )
 
     class Meta:
         model = User
-        fields = ('username', 'face_image', )
+        fields = ('username', 'face_image', 'password1', 'password2')
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(SignupForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
