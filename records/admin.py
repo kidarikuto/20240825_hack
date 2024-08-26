f78e44f759fb5f6d6548acd9546732f921a5bb9c
harune-pg <h.makino.22.6.533@4nonome.com>
2024-08-26 10:17:57 +0900

    管理画面を調整

--- records/admin.py
+++ records/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import EntryExitLog
 
+
+class EntryExitLogAdmin(admin.ModelAdmin):
+    fields = ("user", "action", "timestamp")
+
+
 admin.site.register(EntryExitLog)
